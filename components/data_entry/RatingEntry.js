731e9529fbbfdd062451c63491e2216d3e3bf374
Steven Koontz <smkoontz1@gmail.com>
2020-11-21 12:00:37 -0600

    Removed inner flex styling on rating entry so keyboard avoiding view works

--- components/data_entry/RatingEntry.js
+++ components/data_entry/RatingEntry.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, KeyboardAvoidingView, Platform } from 'react-native';
+import { StyleSheet, View, Text, Button, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import NumericInput from 'react-native-numeric-input';
 import { TextInput } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -14,42 +14,45 @@ const RatingEntry = (props) => {
 
   return (
 		<KeyboardAvoidingView
-			style={{ flex: 1 }}
 			behavior={Platform.OS === 'ios' ?  'padding' : null}
+			style={{ flex: 1 }}
 		>
 			<SafeAreaView style={styles.container}>
-				<View style={styles.inner}>
-					<View style={styles.ratingContainer}>
-						<Text style={styles.description}>Rate your satisfaction on a 10 point scale with this aspect of your life:</Text>
-						<Text style={styles.ratingCategory}>{props.ratingCategory}</Text>
-						<View style={styles.numericInputContainer}>
-							<NumericInput
-								style={styles.numericInput}
-								initValue={1}
-								onChange={value => numValue = value}
-								rounded
-								minValue={1}
-								maxValue={10}
-								step={1}
+				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+					<View style={styles.inner}>
+						<View style={styles.ratingContainer}>
+							<Text style={styles.description}>Rate your satisfaction on a 10 point scale with this aspect of your life:</Text>
+							<Text style={styles.ratingCategory}>{props.ratingCategory}</Text>
+							<View style={styles.numericInputContainer}>
+								<NumericInput
+									style={styles.numericInput}
+									initValue={1}
+									onChange={value => numValue = value}
+									rounded
+									minValue={1}
+									maxValue={10}
+									step={1}
+								/>
+							</View>
+						</View>
+						<View style={styles.commentContainer}>
+							<Text style={styles.commentDescription}>Optionally, add a comment on how you can improve your score in this area over the next week:</Text>
+							<TextInput
+								style={styles.comment}
+								multiline
+								numberOfLines={4}
+								textAlignVertical="top"
 							/>
 						</View>
+						<View style={styles.buttonContainer}>
+							<Button
+								onPress={onClick.bind(this)}
+								title="Next"
+							/>
+						</View>
+						<View style={{ flex : 1 }} />
 					</View>
-					<View style={styles.commentContainer}>
-						<Text style={styles.commentDescription}>Optionally, add a comment on how you can improve your score in this area over the next week:</Text>
-						<TextInput
-							style={styles.comment}
-							multiline
-							numberOfLines={4}
-							textAlignVertical="top"
-						/>
-					</View>
-					<View style={styles.buttonContainer}>
-						<Button
-							onPress={onClick.bind(this)}
-							title="Next"
-						/>
-					</View>
-				</View>
+				</TouchableWithoutFeedback>
 			</SafeAreaView>
 		</KeyboardAvoidingView>
   )
@@ -60,8 +63,6 @@ export default RatingEntry;
 const styles = StyleSheet.create({
   container: {
 		flex: 1,
-		// marginLeft: 25,
-		// marginRight: 25,
 	},
 	inner: {
 		padding: 25,
@@ -73,7 +74,6 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 	},
 	ratingContainer: {
-		flex: 2,
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
@@ -81,10 +81,10 @@ const styles = StyleSheet.create({
 		fontSize: 25,
 		fontWeight: 'bold',
 		textAlign: 'center',
-		marginTop: 40,
+		margin: 40,
 	},
 	numericInputContainer: {
-		marginTop: 20,
+		marginBottom: 50,
 	},
 	commentDescription: {
 		textAlign: 'center',
@@ -92,7 +92,6 @@ const styles = StyleSheet.create({
 		marginBottom: 20,
 	},
 	commentContainer: {
-		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
@@ -102,8 +101,8 @@ const styles = StyleSheet.create({
 		width: 300,		
 	},
   buttonContainer: {
-    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+		alignItems: 'center',
+		margin: 50,
   },
 });
--- screens/TestKeyboardAvoidingView.js
+++ screens/TestKeyboardAvoidingView.js
@@ -5,47 +5,61 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const TestKeyboardAvoidingView = () => {
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS == "ios" ? "padding" : "height"}
-      style={styles.container}
-    >
+			behavior={Platform.OS === "ios" ? "padding" : null}
+			style={{ flex: 1 }}
+		>
 			<SafeAreaView style={styles.container}>
-				<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-					<View style={styles.inner}>
-						<Text style={styles.header}>Header</Text>
-						<TextInput placeholder="Username" style={styles.textInput} />
-						<View style={styles.btnContainer}>
-							<Button title="Submit" onPress={() => null} />
-						</View>
-					</View>
-				</TouchableWithoutFeedback>
+					<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+							<View style={styles.inner}>
+									<Text style={styles.header}>
+											Header
+									</Text>
+									<TextInput
+											placeholder="Username"
+											style={styles.input}
+									/>
+									<TextInput
+											placeholder="Password"
+											style={styles.input}
+									/>
+									<TextInput
+											placeholder="Confrim Password"
+											style={styles.input}
+									/>
+									<View style={styles.btnContainer}>
+											<Button title="Submit" onPress={() => null} />
+									</View>
+									<View style={{ flex : 1 }} />
+							</View>
+					</TouchableWithoutFeedback>
 			</SafeAreaView>
-    </KeyboardAvoidingView>
+		</KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  inner: {
-    padding: 24,
-    flex: 1,
-    justifyContent: "space-around"
-  },
-  header: {
-    fontSize: 36,
-    marginBottom: 48
-  },
-  textInput: {
-    height: 40,
-    borderColor: "#000000",
-    borderBottomWidth: 1,
-    marginBottom: 36
-  },
-  btnContainer: {
-    backgroundColor: "white",
-    marginTop: 12
-  }
+	container: {
+		flex: 1,
+	},
+	inner: {
+		padding: 24,
+		flex: 1,
+		justifyContent: "flex-end",
+	},
+	header: {
+		fontSize: 36,
+		marginBottom: 48,
+	},
+	input: {
+		height: 40,
+		borderColor: "#000000",
+		borderBottomWidth: 1,
+		marginBottom: 36,
+	},
+	btnContainer: {
+		backgroundColor: "white",
+		marginTop: 12,
+	},
 });
 
 export default TestKeyboardAvoidingView;
